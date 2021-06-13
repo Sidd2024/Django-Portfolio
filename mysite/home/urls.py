3f8cdcf6ad3f0561344d607d47d6b5beb4ddb68c
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-06-13 01:20:01 -0400

    Customize admin site

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 from . import views
 from .views import ContactCreate, thanks
 from django.conf.urls.static import static
+from home import views
 
+# Django admin customization
+
+admin.site.site_header = "Tech Lady Juwana"
+admin.site.site_title = "Welcome to the Dashboard"
+admin.site.index_title = "Welcome to the Admin Portal"
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/', views.blog, name='blog'),
