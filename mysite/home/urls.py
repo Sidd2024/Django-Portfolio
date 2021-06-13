e0f5f67693222410c80afd501714c0235b354c3c
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-06-12 22:26:40 -0400

    Update URLS.py

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import ContactCreate, thanks
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/', views.blog, name='blog')
+    path('blog/', views.blog, name='blog'),
+    path('contact/', ContactCreate.as_view(), name='contact'),
+    path('thanks/', thanks, name='thanks')
 ]
