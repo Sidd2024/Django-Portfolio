6e275ae3ccc781b3ab5dd04d875e2ba41416bddc
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2021-06-12 22:27:07 -0400

    Update views.py

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from django.views.generic import CreateView
+from .models import Contact
+from django.urls import reverse_lazy
+from .forms import ContactForm
+# from django.core.mail import send_mail, BadHeaderError, EmailMessage
 
 # Create your views here.
 def home(request):
     # context = {'name': 'Juwana', 'Portfolio': 'Django'}
     return render(request, 'home.html')
+
+
+class ContactCreate(CreateView):
+    model = Contact
+    form_class = ContactForm
+    success_url = reverse_lazy("thanks")
+
+
+def thanks(request):
+    return HttpResponse("Thank you! Will get in touch soon.")
+
+
+# def contact(request):
+#     if request.method=="POST":
+#         # form = ContactForm(data=request.POST)
+#         # if form.is_valid():
+#         name = request.POST['name']
+#         email = request.POST['email']
+#         message = request.POST['message']
+#         print(name, email, message)
+
+        # Email the profile with the contact information
+    #         send_mail(name, message, email, ['testmail@gmail.com'], fail_silently=False)
+
+
+    #         return HttpResponseRedirect('/contact')
+    # else:
+    #     form = ContactForm()
+
+
+    # return render(request, 'contact.html')
 
 def blog(request):
     # return HttpResponse("This is my blog page created by Juwana (/blog)")
