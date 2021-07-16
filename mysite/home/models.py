4a113132d58fc3f1632612e4016ef6bfcc7a4964
Juwana Zerman <juwanazerman@gmail.com>
2021-07-15 22:36:23 -0400

    Update models.py for home

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -19,6 +19,8 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    objects = models.Manager()
+
     class Meta:
         ordering = ['-created_on']
 
