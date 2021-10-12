f51bd362a7e1cac706019bb98af17671358419dc
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 19:04:02 -0400

    Add published date to admin

--- home/admin.py
+++ home/admin.py
@@ -4,7 +4,7 @@ from .models import Post
 
 # Customize Blog Post Data
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on', 'published_date')
     list_filter = ('status',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
