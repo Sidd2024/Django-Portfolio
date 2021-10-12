e3e34a2c397ede8379564a6547666a43211bed25
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 01:41:58 -0400

    Update settings.py static storage line

--- mysite/settings.py
+++ mysite/settings.py
@@ -151,10 +151,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR / "static", './css/style.css' ]
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DISABLE_COLLECTSTATIC = True
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'testemail@gmail.com'
 EMAIL_HOST_USER = ''
