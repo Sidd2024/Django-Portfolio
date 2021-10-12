ec892d0ebd3544d5792c5e7a3e6541594bb99cec
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 00:31:11 -0400

    Return wsgi settings to original

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,10 +10,9 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from mysite.wsgi import mysite
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 
-application = get_wsgi_application(mysite)
+application = get_wsgi_application()
