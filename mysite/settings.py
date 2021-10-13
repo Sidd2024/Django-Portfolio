e8affa518c9b2ff323e10078747f01db6304d860
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 22:55:14 -0400

    Remove extra whitenoise sttings

--- mysite/settings.py
+++ mysite/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
     'mysite', #'mysite.apps.MysiteConfig',
     'home', #'home.apps.HomeConfig',
     'widget_tweaks',
-    'whitenoise.runserver_nostatic',
+    #'whitenoise.runserver_nostatic',
     #'django-environ-2',
     #'python-decouple',
 ]
@@ -151,15 +151,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+#STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #DISABLE_COLLECTSTATIC = True
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
-WHITENOISE_USE_FINDERS = True
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_ALLOW_ALL_ORIGINS = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'testemail@gmail.com'
