65c27742834739856d61647886a40e9d4b35df70
Juwana Zerman <juwanazerman@gmail.com>
2021-10-13 01:41:36 -0400

    Update url pattern

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,7 @@ from django.contrib.staticfiles.views import serve
 from django.conf.urls.static import static
 from django.conf import settings
 from django.core.mail import send_mail, BadHeaderError
+from django.conf.urls.defaults import patterns
 
 
 # Add this after the rest of your urlpatterns. (Ensure you don't have another url defined for your static files
@@ -20,6 +21,9 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     #urlpatterns += static(settings.STATIC_URL)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT})
+    )
