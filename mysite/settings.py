2ac00cc19b8e7743977a3e81473ad585914dfc79
Juwana Zerman <juwanazerman@gmail.com>
2021-10-13 00:06:59 -0400

    Update style.css filepath

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,7 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static', '/static/css/style.css')),)
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #DISABLE_COLLECTSTATIC = True
