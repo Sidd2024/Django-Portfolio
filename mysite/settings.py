f28a8ace587fe768e66ed6b211d812e974558500
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 22:57:20 -0400

    Remove whitenoise setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,9 +153,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #DISABLE_COLLECTSTATIC = True
-#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
