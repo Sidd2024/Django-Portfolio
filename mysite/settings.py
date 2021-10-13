671ae0ff7be3ef2ec71b43eeae069d12835e6ac7
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 22:31:22 -0400

    Update collectstatic

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,11 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static", 'admin', '/css/style.css') ]
-STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static") ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #DISABLE_COLLECTSTATIC = True
-#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'testemail@gmail.com'
@@ -166,7 +166,7 @@ EMAIL_PORT = 1025
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Activate Django-Heroku
-django_heroku.settings(config=locals(), staticfiles=False)
+django_heroku.settings(config=locals()) # , staticfiles=False)
 
 LOGGING = {
     'version': 1,
