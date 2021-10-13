4aab2172be1e49c2333b5e70bb4e5fdf90805de4
Juwana Zerman <juwanazerman@gmail.com>
2021-10-12 23:58:07 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,19 +55,20 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'mysite', #'mysite.apps.MysiteConfig',
     'home', #'home.apps.HomeConfig',
     'widget_tweaks',
-    #'whitenoise.runserver_nostatic',
+
     #'django-environ-2',
     #'python-decouple',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -151,11 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static", '/static/css/style.css') ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #DISABLE_COLLECTSTATIC = True
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
